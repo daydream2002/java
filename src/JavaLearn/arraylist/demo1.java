32733bc57730c4016e690e03cd3fbf30399510fc
daydream <32980275+daydream1024@users.noreply.github.com>
2023-10-05 21:35:30 +0800

    Changes

--- src/JavaLearn/arraylist/demo1.java
+++ src/JavaLearn/arraylist/demo1.java
@@ -1,7 +1,25 @@
 package JavaLearn.arraylist;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class demo1 {
     public static void main(String[] args) {
-        System.out.println('a'-'0');
+        ArrayList<String> list=new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+        list.add("dddd");
+        System.out.println(list);
+        list.remove("aaa");
+        System.out.println(list.remove("aa"));
+        System.out.println(list);
+        list.remove(0);
+        System.out.println(list);
+        list.set(1,"eee");
+        System.out.println(list);
+        String sn = list.get(0);
+        System.out.println(sn);
+        System.out.println(list.size());
     }
 }
---
+++ src/JavaLearn/arraylist/demo2.java
@@ -0,0 +1,12 @@
+package JavaLearn.arraylist;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class demo2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        System.out.println(list);
+    }
+}
