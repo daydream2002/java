ece31221974de75ed921a495477603595b3a2769
daydream <32980275+daydream1024@users.noreply.github.com>
2023-11-07 10:10:48 +0800

    Changes

--- src/month11/Math1.java
+++ src/month11/Math1.java
@@ -2,6 +2,8 @@ package month11;
 
 public class Math1 {
     public static void main(String[] args) {
-        System.out.println(Math.abs(-22));
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+        }
     }
 }
