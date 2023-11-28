595f4cd335e4fdbfe6e48a84924883cbee8f3e5d
daydream <32980275+daydream1024@users.noreply.github.com>
2023-11-28 16:21:14 +0800

    Changes

--- src/month11/Test.java
+++ src/month11/Test.java
@@ -1,6 +1,10 @@
 package month11;
 
+import cn.hutool.core.io.FileUtil;
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.function.IntFunction;
 import java.util.zip.ZipEntry;
@@ -8,37 +12,16 @@ import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Test {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        File src = new File("D:\\desktop\\java\\java\\zip");
-        File dest = new File(src.getParentFile() ,src.getName() + ".zip");
-        System.out.println(dest);
-        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(dest));
-        toZip(src,zos,"");
-        zos.close();
+    public static void main(String[] args) throws IOException {
+        File file = FileUtil.file("D:\\desktop\\java\\java\\a\\a\\a.txt");
+        File touch = FileUtil.touch(file);
+        //System.out.println(touch);
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list,"aaa","bbb","ccc");
+        File file1 = FileUtil.writeLines(list, file, "UTF-8");
+        FileUtil.appendLines(list,file, StandardCharsets.UTF_8);
 
-    }
-    public static void toZip(File src,ZipOutputStream zos,String filename) throws IOException {
-        File[] files = src.listFiles();
-        for (File file : files) {
-            if (file.isFile()){
-                ZipEntry entry = new ZipEntry(filename+"\\"+file.getName());
-                System.out.println(entry);
-                FileInputStream fis = new FileInputStream(file);
-                zos.putNextEntry(entry);
-                int i;
-                while ((i=fis.read())!=-1){
-                    zos.write(i);
-                }
-                fis.close();
-                zos.closeEntry();
-            }else {
-                ZipEntry entry = new ZipEntry(file.toString());
-                zos.putNextEntry(entry);
-                System.out.println(file);
-                toZip(file,zos,filename);
-            }
-
-        }
+        List<String> list1 = FileUtil.readLines(file, StandardCharsets.UTF_8);
+        System.out.println(list1);
     }
 }
-
